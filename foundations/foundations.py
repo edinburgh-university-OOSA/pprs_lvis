e573f0893e6c2f4fcc3ad35ca6272fce9ef68f45
sthancock <steven.hancock@ed.ac.uk>
2019-11-18 15:03:32 +0000

    Initial commit.

---
+++ foundations/foundations.py
@@ -0,0 +1,29 @@
+
+
+'''
+Examples of the basic functions of python3
+'''
+
+
+# Comments can be behind "#"
+'''Or behind three quotation marks'''
+# use # for comments in text, and ''' for descriptions of functions
+
+
+
+# print something to the screen
+print("Welcome to python3")
+
+# set a variable
+x=2.718281828459045
+
+# write variable to screen
+print(x)
+
+# set a "string", ie a word. Note we have overwritten "x" set earlier and changed it from a number to a word
+x="This is a word"
+
+# print a string variable to the screen
+print(x)
+
+
