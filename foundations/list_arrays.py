a395c686fab837d8fa86be6387676642035dc307
sthancock <steven.hancock@ed.ac.uk>
2019-11-18 15:24:36 +0000

    List arrays finished.

---
+++ foundations/list_arrays.py
@@ -0,0 +1,73 @@
+
+
+'''
+Basics of lists and 
+arrays in python3
+'''
+
+
+# We can also store lists of things in a single variable
+x=[1,2,3,4,5]
+
+# This can be used to pass around large amounts of data without needing thousands of variables
+print(x)
+
+# it can be accessed by telling pythong with "element" you want
+print(x[0])
+
+
+# Note that the elements start from "0" and go, in this case, to 4.
+print(x[0],x[4])
+
+
+# we can reassign elements one by one
+x[2]=4.243546
+print(x)
+
+
+# or overwrite the whole thing
+x=[4,3,2,1]
+
+
+# lists can be of any type
+x=["This","is","now","a","string"]
+
+
+# or even a mix
+x=[1,2,"sausage",4,5]
+print(x)
+
+
+# there are many other ways to hold large amounts of data in pythom., eg. dictionaries and tuples
+# but lists are perhaps the most common
+
+# in GIS and EO, we often want to hold large arrays of numbers
+# this can be efficiently done using a "numpy" array, which have
+# been specifically designed for this. Import numpy
+
+import numpy as np
+
+
+# set some data (most often we will read from file)
+x=np.array([4,3,2,1])
+
+
+# It can be accessed in a similar way to lists
+print(x[0],x[2])
+
+
+# but now we can perform maths on the whole array
+y=x/2
+print("original",x,"modified",y)
+
+
+# arrays can also be multi-dimensional. Let us make one full of "-1"
+x=np.full((100,100),-1)
+
+# the "np.full()" command tells it to make an array full of a given value
+# the "(100,100)" tells it to make it 100*100 elements
+# the "-1" tells it what value to fill the array with
+
+
+# Now we can store large amounts of data in a single variable. Let us see how we can analyse that
+
