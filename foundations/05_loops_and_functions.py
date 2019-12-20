c6aeecaa728390e49309d3da25a26ad51776b6d3
sthancock <steven.hancock@ed.ac.uk>
2019-12-20 11:32:40 +0000

    Examples of objects and ifs added.

--- foundations/05_loops_and_functions.py
+++ foundations/05_loops_and_functions.py
@@ -89,7 +89,50 @@ for i in range(0,len(x)):
     print(x[i],y[i])
 
 
-# We have now had a rapid overview of the main features of python needed to analyse EO data.
-# We can now follow a script written to analyse LVIS lidar data.
 
+# Ifs
+
+# An if statement lets us control the flow of a program, allowing it to do different things in different conditions.
+# This allows our program to make "decisions", making it more flexible
+
+
+# example of an if statement
+
+x=0.4           # a value we will use to test
+threshold=0.5   # a threshold against which we will test
+
+if(x<threshold):   # the test
+  print(x,"is less than",threshold)   # if the test is passed, do this
+else:              # if the test fails
+  print(x,"is greater than or equal to",threshold) # otherwise do this
+
+# python has a datatype called "booleab" for doing this
+
+testPar=True
+
+if(testPar):
+  print("It is true")
+else:
+  print("It is false")
+
+
+# we can stack if statements using "else if" statement, "elif"
+
+
+x=0.4
+thresh1=0.25   # a threshold against which we will test
+thresh2=0.5   # a threshold against which we will test
+thresh3=0.75   # a threshold against which we will test
+
+if(x<thresh1):   # the test
+  print(x,"is less than",thresh1)   # if the test is passed, do this
+elif(x<thresh2):   # otherwise this test
+  print(x,"is less than",thresh2)   # if the test is passed, do this
+elif(x<thresh3):   # otherwise this test
+  print(x,"is less than",thresh3)   # if the test is passed, do this
+else:              # if the test fails
+  print(x,"is greater than or equal to",thresh3) # otherwise do this
+
+
+# We have covered the basic aspects of python. We can now try applying them to some remote sensing data.
 
---
+++ foundations/06_objects.py
@@ -0,0 +1,59 @@
+
+
+'''
+An example of an object
+'''
+
+
+# Often we want to store groups of data together to make it easier to pass it around our program
+# We may also want to define functions which we will only ever apply to these grouped datasets
+# To allow this, sets of data and functions can be "encapsulated" in to a group, known as an object (also called a class)
+# This is particularly useful as programs become larger, and makes them easier to manage.
+
+
+# Much of the code we use is made up of classes.
+# We do not need to create our own classes in this course
+# But every package we use is a class, so having some
+# insight in to what a class is will help us understand
+# how to make use of these packages.
+
+
+# define a new class
+
+class testClass():
+  '''
+  This is a class to illustrate
+  how classes work
+  '''
+  # This function is called when the class is first applied
+  # it initialises the class
+  def __init__(self,somedata,moredata):  # we pass two items to store in the class
+    '''The class initialiser'''
+    self.x=somedata  # here "self" is the instance of the clasa.s The "." means there is something inside that class
+    self.y=moredata  # in this case, we have saved two pieces of data, x and y. We have already used the "." when using numpy etc.
+  # a function we would like to use. "Self" contains the data
+  def multiplyContents(self):
+    '''Function to multiply the contents together'''
+    z=self.x*self.y   # note that z is not in self, so is not saved after this function
+    print("The multiplitcation of",self.x,"and",self.y,"is",z)
+
+
+
+# call an instance of that class
+class1=testClass(3,4)   # this calls __init__ and passes 3 and 4 to be saved as x and y
+
+# call the function
+class1.multiplyContents()
+
+# we can directly access the data too
+print("Data within is",class1.x,class1.y)
+
+# We can create a separate instance of the class, which will be independent from class1
+class2=testClass(10,42)   # this calls __init__ and passes 3 and 4 to be saved as x and y
+
+
+# to prove they are independent
+class1.multiplyContents()
+class2.multiplyContents()
+
+
